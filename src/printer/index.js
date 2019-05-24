e69dbdff960838bafa9b6e78abf77f483844be80
fisker <lionkay@gmail.com>
2019-05-25 00:15:48 +0800

    feat: print as json5

--- src/printer/index.js
+++ src/printer/index.js
@@ -1,4 +1,5 @@
 export {default as json} from './json'
+export {default as json5} from './json5'
 export {default as yaml} from './yaml'
 export {default as toml} from './toml'
 export {default as cjs} from './cjs'
