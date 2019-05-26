dac5147496c09832a7a91934f2060ad99d55c931
fisker <lionkay@gmail.com>
2019-05-26 14:33:04 +0800

    fix: fix esm options

--- src/loader/esm-require.js
+++ src/loader/esm-require.js
@@ -2,12 +2,8 @@ export default require('esm')(module, {
   cjs: {
     cache: false,
     extensions: false,
-    interop: false,
     mutableNamespace: false,
     namedExports: false,
-    paths: false,
     vars: true,
   },
-  force: true,
-  mode: 'all',
 })
