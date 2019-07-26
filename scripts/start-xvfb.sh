a3edbd47f4fb16b613629ab31dff831b7e4d5460
fisker <lionkay@gmail.com>
2019-07-26 16:25:19 +0800

    ci: fix sh

--- scripts/start-xvfb.sh
+++ scripts/start-xvfb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export DISPLAY=:99.0;
+echo "start-xvfb.sh"
+export DISPLAY=:99.0
 sudo cp scripts/xvfb.init /etc/init.d/xvfb
 sudo chmod +x /etc/init.d/xvfb
 sudo update-rc.d xvfb defaults
