1e94321aa39df6126ea7d784c0cd058fd0dd431c
fisker <lionkay@gmail.com>
2019-07-26 16:52:19 +0800

    ci: test

--- scripts/start-xvfb.sh
+++ scripts/start-xvfb.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-export DISPLAY=:99.0
-sh -e scripts/xvfb.init start
+# echo "start-xvfb.sh"
+# export DISPLAY=:99.0
+# sudo cp scripts/xvfb.init /etc/init.d/xvfb
+# sudo chmod +x /etc/init.d/xvfb
+# sudo update-rc.d xvfb defaults
+# sudo service xvfb start
+cd /etc/init.d/
+ls
+# echo "start-xvfb.sh:end"
